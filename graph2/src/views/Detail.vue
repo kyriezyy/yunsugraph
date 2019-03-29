c5cf84f32bd63a5ca3fd329b52d340e40fcb419e
chenli <chenli90s@qq.com>
2019-03-29 11:21:49 +0800

    添加详情页搜索框

--- graph2/src/views/Detail.vue
+++ graph2/src/views/Detail.vue
@@ -1,141 +1,145 @@
 <template>
-  <div class="container">
-    <div class="main">
-      <div class="box-content">
-        <!--<div class="graph-box" id="graph">-->
+  <div class="detail">
+
+    <header-search/>
+    <div class="container">
+      <div class="main">
+        <div class="box-content">
+          <!--<div class="graph-box" id="graph">-->
           <!--<graph/>-->
-        <!--</div>-->
-        <div class="info-box">
-          <div class="section" id="jibenxinxi">
-            <div class="section-title">基本信息</div>
-            <div class="content">
-              <div class="params-box">
-                <div class="param-item" v-for="(value,key) in detail.product_info" :key="key">
-                  <span class="key">{{key}}：</span>
-                  <span class="value">{{value}}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="section" id="wuguanxingzhi">
-            <div class="section-title">物化性质</div>
-            <div class="content">
-              <div class="params-box">
-                <div class="param-item" v-for="(value,key) in detail.detail_quality" :key="key">
-                  <span class="key">{{key}}</span>
-                  <span class="value">{{value}}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="section" id="graph">
-            <div class="section-title">图谱</div>
-            <div class="content">
-              <div class="graph-box">
-                <graph/>
-              </div>
-            </div>
-          </div>
-          <div class="section" id="anquanxinxi">
-            <div class="section-title">安全信息</div>
-            <div class="content">
-              <div class="params-box">
-                <div class="param-item" v-for="(value,key) in detail.safe_info" :key="key">
-                  <span class="key">{{key}}：</span>
-                  <span class="value">{{value}}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="section" id="hechengluxian">
-            <div class="section-title">合成路线</div>
-            <div class="content">
-              <div class="params-box">
-                <div class="param-item" v-for="(value,key) in guanxi.data.synts" :key="key">
-                  <div class="huaxuepin front">
-                    <div
-                      class="img"
-                      :style="{background:`url(${value.front[0].url}) no-repeat center center/contain`}"
-                    ></div>
-                    <p class="huaxuepin-cas">{{value.front[0].cas}}</p>
-                    <p class="huaxuepin-name">{{value.front[0].name}}</p>
-                  </div>
-                  <div class="percent">
-                    <div class="jiantou"></div>
-                    <p>{{value.pre}}</p>
-                  </div>
-                  <div class="huaxuepin back">
-                    <div
-                      class="img"
-                      :style="{background:`url(${value.back[0].url}) no-repeat center center/contain`}"
-                    ></div>
-                    <p class="huaxuepin-cas">{{value.back[0].cas}}</p>
-                    <p class="huaxuepin-name">{{value.back[0].name}}</p>
+          <!--</div>-->
+          <div class="info-box">
+            <div class="section" id="jibenxinxi">
+              <div class="section-title">基本信息</div>
+              <div class="content">
+                <div class="params-box">
+                  <div class="param-item" v-for="(value,key) in detail.product_info" :key="key">
+                    <span class="key">{{key}}：</span>
+                    <span class="value">{{value}}</span>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="section" id="shangxiayouchanpin">
-            <div class="section-title">上下游产品</div>
-            <div class="content">
-              <div class="params-box">
-                <div class="params-title">上游产品
-                  <span class="more">更多</span>
-                </div>
-                <div class="params-content">
-                  <div class="huaxuepin front" v-for="item in guanxi.data.updown.ups" :key="item.cas">
-                    <div
-                      class="img"
-                      :style="{background:`url(${item.url}) no-repeat center center/contain`}"
-                    ></div>
-                    <p class="huaxuepin-cas">{{item.cas}}</p>
-                    <p class="huaxuepin-name">{{item.name}}</p>
-                  </div>
-                </div>
-              </div>
-              <div class="params-box">
-                <div class="params-title">下游产品
-                  <span class="more">更多</span>
-                </div>
-                <div class="params-content">
-                  <div class="huaxuepin front" v-for="item in guanxi.data.updown.downs" :key="item.cas">
-                    <div
-                      class="img"
-                      :style="{background:`url(${item.url}) no-repeat center center/contain`}"
-                    ></div>
-                    <p class="huaxuepin-cas">{{item.cas}}</p>
-                    <p class="huaxuepin-name">{{item.name}}</p>
+            <div class="section" id="wuguanxingzhi">
+              <div class="section-title">物化性质</div>
+              <div class="content">
+                <div class="params-box">
+                  <div class="param-item" v-for="(value,key) in detail.detail_quality" :key="key">
+                    <span class="key">{{key}}</span>
+                    <span class="value">{{value}}</span>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="section" id="sds">
-            <div class="section-title">SDS</div>
-            <div class="content">
-              <div v-html="detail.sds.sds"></div>
+            <div class="section" id="graph">
+              <div class="section-title">图谱</div>
+              <div class="content">
+                <div class="graph-box">
+                  <graph/>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="section" id="msds">
-            <div class="section-title">MSDS</div>
-            <div class="content">
-              <div v-html="detail.msds"></div>
+            <div class="section" id="anquanxinxi">
+              <div class="section-title">安全信息</div>
+              <div class="content">
+                <div class="params-box">
+                  <div class="param-item" v-for="(value,key) in detail.safe_info" :key="key">
+                    <span class="key">{{key}}：</span>
+                    <span class="value">{{value}}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="section" id="hechengluxian">
+              <div class="section-title">合成路线</div>
+              <div class="content">
+                <div class="params-box">
+                  <div class="param-item" v-for="(value,key) in guanxi.data.synts" :key="key">
+                    <div class="huaxuepin front">
+                      <div
+                        class="img"
+                        :style="{background:`url(${value.front[0].url}) no-repeat center center/contain`}"
+                      ></div>
+                      <p class="huaxuepin-cas">{{value.front[0].cas}}</p>
+                      <p class="huaxuepin-name">{{value.front[0].name}}</p>
+                    </div>
+                    <div class="percent">
+                      <div class="jiantou"></div>
+                      <p>{{value.pre}}</p>
+                    </div>
+                    <div class="huaxuepin back">
+                      <div
+                        class="img"
+                        :style="{background:`url(${value.back[0].url}) no-repeat center center/contain`}"
+                      ></div>
+                      <p class="huaxuepin-cas">{{value.back[0].cas}}</p>
+                      <p class="huaxuepin-name">{{value.back[0].name}}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="section" id="shangxiayouchanpin">
+              <div class="section-title">上下游产品</div>
+              <div class="content">
+                <div class="params-box">
+                  <div class="params-title">上游产品
+                    <span class="more">更多</span>
+                  </div>
+                  <div class="params-content">
+                    <div class="huaxuepin front" v-for="item in guanxi.data.updown.ups" :key="item.cas">
+                      <div
+                        class="img"
+                        :style="{background:`url(${item.url}) no-repeat center center/contain`}"
+                      ></div>
+                      <p class="huaxuepin-cas">{{item.cas}}</p>
+                      <p class="huaxuepin-name">{{item.name}}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="params-box">
+                  <div class="params-title">下游产品
+                    <span class="more">更多</span>
+                  </div>
+                  <div class="params-content">
+                    <div class="huaxuepin front" v-for="item in guanxi.data.updown.downs" :key="item.cas">
+                      <div
+                        class="img"
+                        :style="{background:`url(${item.url}) no-repeat center center/contain`}"
+                      ></div>
+                      <p class="huaxuepin-cas">{{item.cas}}</p>
+                      <p class="huaxuepin-name">{{item.name}}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="section" id="sds">
+              <div class="section-title">SDS</div>
+              <div class="content">
+                <div v-html="detail.sds.sds"></div>
+              </div>
+            </div>
+            <div class="section" id="msds">
+              <div class="section-title">MSDS</div>
+              <div class="content">
+                <div v-html="detail.msds"></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="nav-box">
-      <a class="nav-item add-color" @click="navTo('jibenxinxi',0)">基本信息</a>
-      <a class="nav-item" @click="navTo('wuguanxingzhi',1)">物化性质</a>
-      <a class="nav-item" @click="navTo('graph', 2)">图谱</a>
-      <a class="nav-item" @click="navTo('anquanxinxi', 3)">安全信息</a>
-      <a class="nav-item" @click="navTo('hechengluxian', 4)">合成路线</a>
-      <a class="nav-item" @click="navTo('shangxiayouchanpin', 5)">上下游产品</a>
-      <a class="nav-item" @click="navTo('sds', 6)">SDS</a>
-      <a class="nav-item" @click="navTo('msds', 7)">MSDS</a>
+      <div class="nav-box">
+        <a class="nav-item add-color" @click="navTo('jibenxinxi',0)">基本信息</a>
+        <a class="nav-item" @click="navTo('wuguanxingzhi',1)">物化性质</a>
+        <a class="nav-item" @click="navTo('graph', 2)">图谱</a>
+        <a class="nav-item" @click="navTo('anquanxinxi', 3)">安全信息</a>
+        <a class="nav-item" @click="navTo('hechengluxian', 4)">合成路线</a>
+        <a class="nav-item" @click="navTo('shangxiayouchanpin', 5)">上下游产品</a>
+        <a class="nav-item" @click="navTo('sds', 6)">SDS</a>
+        <a class="nav-item" @click="navTo('msds', 7)">MSDS</a>
+      </div>
     </div>
   </div>
 </template>
@@ -144,11 +148,14 @@ import detail from '../jsons/detail.json'
 import guanxi from '../jsons/guanxi.json'
 
 import Graph from './Graph'
+import HeaderSearch from '../components/HeaderSearch'
+
 // console.log(detail)
 export default {
   name: 'detail',
   components: {
-    Graph
+    Graph,
+    HeaderSearch
   },
   data () {
     return {
@@ -169,9 +176,13 @@ export default {
 }
 </script>
 <style scoped>
-  .container {
-    height: calc(100% - 1px);
 
+  .detail{
+    height: calc(100% - 1px);
+  }
+
+  .container {
+    height: calc(100% - 52px);
     display: flex;
     width: 100%;
   }
